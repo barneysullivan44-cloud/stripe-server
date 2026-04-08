e41a2b5af4c5f06a26e5084281585872ba119a40
barneysullivan44-cloud <barneysullivan44@gmail.com>
2026-04-08 18:58:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ import cors from 'cors';
 import Stripe from 'stripe';
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-// CREATE CHECKOUT SESSION
 app.post('/create-checkout-session', async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.create({
@@ -31,18 +31,19 @@ app.post('/create-checkout-session', async (req, res) => {
     });
 
     res.json({ url: session.url });
+
   } catch (error) {
-    console.log(error);
+    console.log('ERROR:', error);
     res.status(500).json({ error: error.message });
   }
 });
 
-// TEST ROUTE
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
 const PORT = process.env.PORT || 10000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
