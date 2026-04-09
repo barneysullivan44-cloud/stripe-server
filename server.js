29bb4b2deb4076d59532276fbd272c41baab4eaa
barneysullivan44-cloud <barneysullivan44@gmail.com>
2026-04-08 23:28:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -44,6 +44,9 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
+});
 });
